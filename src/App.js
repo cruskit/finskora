4e2953992d99833143b4b5ec694554af20b2ea9f
Paul Ruskin <cruskit@gmail.com>
2021-01-17 11:48:43 +1100

    Changed app from funtional component to a class.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload - again.
         </p>
         <a
           className="App-link"
@@ -19,7 +22,10 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
+
 }
+
 
 export default App;
