9e5405bf8ec2f00cb055175db3ce70034165feb8
Paul Ruskin <cruskit@gmail.com>
2021-01-17 21:40:06 +1100

    Added scorepad for score entry and event handler.

--- src/App.js
+++ src/App.js
@@ -4,24 +4,142 @@ import './App.css';
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      players: ['Seb', 'Ewan'],
+      numTurns: 0,
+      turnScores: [ // Just putting in some dummy data for testing
+        [{ score: 5, total: 5 }],
+        [{ score: 10, total: 10 }]
+      ],
+    };
+  }
+
+  handleScoreEntered(userScore) {
+
+    console.log("Handling entered score: " + userScore);
+
+    const currentPlayer = this.state.numTurns % this.state.players.length;
+
+    const newState = JSON.parse(JSON.stringify(this.state));
+
+    const turnScores = this.state.turnScores[currentPlayer];
+    const currentTotal = turnScores[turnScores.length - 1].total;
+
+    newState.turnScores[currentPlayer].push({
+      score: userScore,
+      total: currentTotal + userScore,
+    });
+    newState.numTurns = this.state.numTurns + 1;
+
+
+    this.setState(newState);
+  }
+
   render() {
     return (
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload - again.
+      <div>
+        <header>
+          <p>
+            Scoreboard
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        </header>
+        <div>
+          <ScorePad
+            onScoreEntered={(s) => this.handleScoreEntered(s)}
+          />
+        </div>
+        <div>
+          <PlayerHistory
+            player={this.state.players[0]}
+            scores={this.state.turnScores[0]}
+          />
+          <PlayerHistory
+            player={this.state.players[1]}
+            scores={this.state.turnScores[1]}
+          />
+        </div>
+      </div>
+    );
+  }
+
+}
+
+function EntryButton(props) {
+  return (
+    <button
+      className="entryButton"
+      onClick={props.onClick}
+    >
+      {props.description}
+    </button>
+  );
+}
+
+class ScorePad extends React.Component {
+
+  renderEntryButton(scoreValue) {
+
+    return (
+      <EntryButton
+        onClick={() => this.props.onScoreEntered(scoreValue)}
+        description={scoreValue}
+      />
+    );
+
+  }
+
+  render() {
+
+    return (
+      <div>
+        {this.renderEntryButton(0)}
+        {this.renderEntryButton(1)}
+        {this.renderEntryButton(2)}
+        {this.renderEntryButton(3)}
+        {this.renderEntryButton(4)}
+        {this.renderEntryButton(5)}
+        {this.renderEntryButton(6)}
+        {this.renderEntryButton(7)}
+        {this.renderEntryButton(8)}
+        {this.renderEntryButton(9)}
+        {this.renderEntryButton(10)}
+        {this.renderEntryButton(11)}
+        {this.renderEntryButton(12)}
+      </div>
+
+    );
+  }
+}
+
+
+class PlayerHistory extends React.Component {
+
+  renderScores() {
+    const scoreList = this.props.scores.map((scores, round) => {
+
+      return (
+        < li key={round} >
+          {scores.total} | {scores.score}
+        </li >
+      );
+
+    });
+    return scoreList;
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.props.player}</p>
+        <ul>
+          {this.renderScores()}
+        </ul>
+
+      </div>
     );
   }
 
