69df3d572863a6d683da8870ca3b593e68cfa398
Paul Ruskin <cruskit@gmail.com>
2021-01-26 16:10:25 +1100

    Added support for re-ordering the player list and deleting players.

--- src/PlayerSelection.js
+++ src/PlayerSelection.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './iconoo.min.css';
 import './PlayerSelection.css';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button'
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 class PlayerSelection extends React.Component {
 
     constructor(props) {
@@ -13,30 +14,58 @@ class PlayerSelection extends React.Component {
         this.state = {
             selectedPlayers: playerNames,
             recentPlayers: Array(0),
+            newPlayerName: "",
         }
     }
 
     displaySelectedPlayers() {
         const playerList = this.state.selectedPlayers.map((name, number) => {
             return (
-                <p key={name}>{name}</p>
+                <p key={name}>
+                    {name}
+                    &nbsp; <span className="iconoo-caretUpCircle"
+                        onClick={() => this.movePlayer(number, -1)}></span>
+                    &nbsp; <span className="iconoo-caretDownCircle"
+                        onClick={() => this.movePlayer(number, 1)}></span>
+                    &nbsp; <span className="iconoo-crossCircle"
+                        onClick={() => this.deletePlayer(number)}></span>
+                </p>
             );
         });
         return playerList;
     }
 
-    addPlayer(){
+    addPlayer() {
+        if (this.state.newPlayerName.length > 0) {
+            const players = this.state.selectedPlayers.slice();
+            players.push(this.state.newPlayerName);
+            this.setState({ selectedPlayers: players, newPlayerName: "" })
+        }
+    }
+
+    deletePlayer(index) {
         const players = this.state.selectedPlayers.slice();
-        players.push("Player " + (players.length + 1));
-        this.setState({selectedPlayers: players})
+        players.splice(index, 1);
+        this.setState({ selectedPlayers: players })
     }
 
-    deletePlayer(){
-        const players = this.state.selectedPlayers.slice(0,this.state.selectedPlayers.length-1);
-        this.setState({selectedPlayers: players})
+    movePlayer(index, numSpots) {
+        const players = this.state.selectedPlayers.slice();
+        const newIndex = index + numSpots;
+        if (newIndex < 0 || newIndex >= players.length){
+            console.log(`Attempt to move players outside array bounds, index: ${index}, numSpots: ${numSpots}`);
+            return;
+        }
+
+        // Swap the players around
+        const prev = players[newIndex];
+        players[newIndex] = players[index];
+        players[index] = prev;
+
+        this.setState({ selectedPlayers: players })
     }
 
-    startGame(){
+    startGame() {
         console.log("Request to start game with players: " + this.state.selectedPlayers);
         this.props.onStartGame(this.state.selectedPlayers);
     }
@@ -50,12 +79,17 @@ class PlayerSelection extends React.Component {
 
                 {this.displaySelectedPlayers()}
 
-                <Button variant="primary" onClick={() => this.addPlayer()}>
-                    Add Player
-                </Button>
-                <Button variant="primary" onClick={() => this.deletePlayer()}>
-                    Delete Player
-                </Button>
+                <Form>
+
+                    <Form.Control size="lg" type="text" placeholder="Player or Team Name"
+                        value={this.state.newPlayerName}
+                        onChange={(e) => this.setState({ newPlayerName: e.target.value })}
+                    />
+                    <Button variant="primary" onClick={() => this.addPlayer()}>
+                        Add Player
+                    </Button>
+                </Form>
+
                 <Button variant="primary" onClick={() => this.startGame()}>
                     Start Game with selected players
                 </Button>
