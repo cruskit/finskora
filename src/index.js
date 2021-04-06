f5ff798b12c4315ebd2dead5cb9d1cac9e9fd1f9
Paul Ruskin <cruskit@gmail.com>
2021-04-06 22:29:12 +1000

    Added support for PWA so we can play offline

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
